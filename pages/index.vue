948c0f48310262013e4f0cbb95ad73fc21a6df5d
K-yuri <dbfl2989@mokwon.ac.kr>
2021-05-26 14:21:22 +0900

    web site upload3

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="hero is-primary">
       <div class="hero-body">
       <p class="title">
-       <i class="fa-solid fa-temperature-three-quarters" />기온별 <i class="fa-solid fa-clothes-hanger" />옷차림
+       <font-awesome-icon icon="fa-solid fa-temperature-three-quarters" />기온별 <i class="fa-solid fa-clothes-hanger" />옷차림
       </p>
       <p class="subtitle">
         계절과 기온에 따른 적당한 옷차림에 대해 설명하는 곳
@@ -13,8 +13,9 @@
     <div class="columns">
       <div class="column">
         <article class="message">
+
           <div class="message-header">
-            28ºC 이상
+            <font-awesome-icon icon="fa-solid fa-temperature-sun" />28ºC 이상
           </div>
           <div class="message-body">
             민소매, 반팔, 반바지, 짧은 치마, 린넨 옷
@@ -33,6 +34,42 @@
           <div class="message-body">
             얇은 가디건, 긴팔, 면바지, 청바지
           </div>
+
+          <div class="message-header">
+            19ºC ~ 17ºC
+          </div>
+          <div class="message-body">
+            얇은 니트, 맨투맨, 가디건, 청바지
+          </div>
+
+          <div class="message-header">
+            16ºC ~ 12ºC
+          </div>
+          <div class="message-body">
+            자켓, 가디건, 야상, 스타킹, 청바지, 면바지
+          </div>
+
+          <div class="message-header">
+            11ºC ~ 9ºC
+          </div>
+          <div class="message-body">
+            자켓, 트렌치코트, 야상, 니트, 청바지, 스타킹
+          </div>
+
+          <div class="message-header">
+            8ºC ~ 5ºC
+          </div>
+          <div class="message-body">
+            코트, 가죽자켓, 히트텍, 니트, 레깅스
+          </div>
+
+          <div class="message-header">
+            <font-awesome-icon icon="fa-solid fa-temperature-snow" />4ºC 이하
+          </div>
+          <div class="message-body">
+            패딩, 두꺼운 코트, 목도리, 기모제품
+          </div>
+
         </article>
       </div>
     </div>
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "web site upload2"
+git commit -m "web site upload3"
 git push -u origin master
