040fabd48c1d29985e66cd0feb09fc1e3e943ce5
K-yuri <dbfl2989@mokwon.ac.kr>
2021-06-22 22:12:17 +0900

    add google map API

--- pages/clothesShop.vue
+++ pages/clothesShop.vue
@@ -60,13 +60,13 @@ import Firebase from "firebase";
 // firebase init - add your own config here
 const firebaseConfig = {
     apiKey: "AIzaSyDVPerHjZIJeBli_U_naMrf4mRig8qsT3I",
-  authDomain: "cloud-clothes.firebaseapp.com",
-  databaseURL: "https://cloud-clothes-default-rtdb.firebaseio.com",
-  projectId: "cloud-clothes",
-  storageBucket: "cloud-clothes.appspot.com",
-  messagingSenderId: "371062655392",
-  appId: "1:371062655392:web:2fb3438a59030dd439fcc5",
-  measurementId: "G-PCNMMGQ2P0"
+    authDomain: "cloud-clothes.firebaseapp.com",
+    databaseURL: "https://cloud-clothes-default-rtdb.firebaseio.com",
+    projectId: "cloud-clothes",
+    storageBucket: "cloud-clothes.appspot.com",
+    messagingSenderId: "371062655392",
+    appId: "1:371062655392:web:2fb3438a59030dd439fcc5",
+    measurementId: "G-PCNMMGQ2P0"
 };
 let app = Firebase.initializeApp(firebaseConfig);
 let db = app.database();
