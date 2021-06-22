17e1c84fb094a0e803466d5b55242137c7c31599
K-yuri <dbfl2989@mokwon.ac.kr>
2021-06-23 04:41:38 +0900

    load last page

--- pages/clothesShop.vue
+++ pages/clothesShop.vue
@@ -20,39 +20,8 @@
         ></div>
       </div>
     </div>
-		<section class="box">
-			<form @submit.prevent="submitDogName">
-				<div class="field">
-					<label class="label">의류</label>
-					<div class="control">
-						<input type="text" v-model="dogName" />
-						<button class="button is-link" type="submit">매장 찾기</button>
-					</div>
-					<p class="help is-success">
-						알아보고 싶은 매장의 이름을 입력하세요.
-					</p>
-				</div>
-			</form>
-		</section>
 	</div>
 </template>
-<script>
-	export default {
-		data() {
-			return {
-				dogNameField: 'dogName',
-				dogName: null,
-			};
-		},
-		methods: {
-			submitDogName() {
-				this.$router.push(
-					'/studyClothes?' + this.dogNameField + '=' + this.dogName
-				);
-			},
-		},
-	};
-</script>
 <script>
 import { Loader } from "google-maps";
 const loader = new Loader("AIzaSyDHWccUDRQNam53tac-qt22c6MOl6TKScI");
